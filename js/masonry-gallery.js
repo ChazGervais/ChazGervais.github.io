b4575880e210f6dfeaf8ee7197b34de1829b7786
AntoniaG <antonia.grillmair@gmail.com>
2021-03-18 14:56:07 +0100

    text fix

--- js/masonry-gallery.js
+++ js/masonry-gallery.js
@@ -6,7 +6,7 @@ if (document.readyState === 'loading') {
 
 async function ready() {
     var gallery = document.querySelector('#gallery');
-    var body = document.querySelector('.body')
+    /*var body = document.querySelector('.body')*/
     var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
     var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
     var resizeAll = function () {
@@ -17,10 +17,32 @@ async function ready() {
             el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
         });
     };
+
+    var counterDispl = 0;
+    var counterAll = 0;
     gallery.querySelectorAll('img').forEach(function (item) {
         item.classList.add('byebye');
-        if (item.complete) {
-            console.log(item.src);
+
+        if (item.complete) {           
+            item.addEventListener('load', function () {                  
+                var altura = getVal(gallery, 'grid-auto-rows');
+                var gap = getVal(gallery, 'grid-row-gap');
+                var gitem = item.parentElement.parentElement;
+                gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+                item.classList.remove('byebye');
+                counterDispl = counterDispl+1; 
+                console.log('Img displayed: ' + counterDispl + item.className);
+            } )
+        } else {
+            console.log(item);
+            counterAll = counterAll+1;
+
+        };
+        var notDispl = counterAll-counterDispl;
+                console.log('Not displayed: ' + notDispl)
+        
+        /*if (item.complete) {
+            console.log(item); 
         }
         else {
             item.addEventListener('load', function () {
@@ -30,7 +52,8 @@ async function ready() {
                 gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
                 item.classList.remove('byebye');
             });
-        };
+        };*/
+
     });
     window.addEventListener('resize', resizeAll);
     gallery.querySelectorAll('.gallery-item').forEach(function (item) {
